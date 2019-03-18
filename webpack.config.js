45350d4e5a7291a46b65bc8f5efa4ced4e5327f4
Artsiom_Reutovich <artsiom_reutovich@epam.com>
2019-03-19 00:01:02 +0300

    [no-ticket]: webpack config refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -1,103 +1,19 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const merge = require('webpack-merge');
+const developmentConfig = require('./webpack/webpack.dev.config');
+const productionConfig = require('./webpack/webpack.prod.config');
+const commonConfig = require('./webpack/webpack.common.config');
 
-module.exports = (env) => {
-  if(env === 'development'){
-    return {
-      context: path.resolve(__dirname, 'src'),
-      mode: "development",
-      devtool: 'source-map', //none,eval
-
-      entry: './init.js',
-      output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-      },
-      watch: true,
-
-      devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9090
-      },
-
-      module: {
-        rules: [
-          {
-            test: /\.js?$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/
-          },
-          {
-            test:/\.scss$/,
-            use: ExtractTextPlugin.extract({
-              fallback: 'style-loader',
-              use: ['css-loader', 'sass-loader']
-            })
-          }
-        ]
-      },
-
-      plugins: [
-        new HtmlWebpackPlugin({
-          title: 'React application',
-          template: './index.html'
-        }),
-        new ExtractTextPlugin({filename: 'style.css'})
-      ],
-
-      optimization: {
-        splitChunks: {
-          cacheGroups: {
-            commons: {
-              name: "commons",
-              chunks: 'all',
-              minSize: 0,
-              minChunks: 2,
-            }
-          }
-        }
-      }
-    }
+module.exports = () => {
+  if(process.env.NODE_ENV === 'development'){
+    return merge([
+      commonConfig(),
+      developmentConfig(),
+    ])
   }
-
-  if(env === "production") {
-    return {
-      context: path.resolve(__dirname, 'src'),
-      mode: "production",
-      devtool: 'eval',
-
-      entry: './init.js',
-      output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'build')
-      },
-
-      module: {
-        rules: [
-          {
-            test: /\.js?$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/
-          },
-          {
-            test:/\.scss$/,
-            use: ExtractTextPlugin.extract({
-              fallback: 'style-loader',
-              use: ['css-loader', 'sass-loader']
-            })
-          }
-        ]
-      },
-
-      plugins: [
-        new HtmlWebpackPlugin({
-          title: 'React application',
-          template: './index.html'
-        }),
-        new ExtractTextPlugin({filename: 'style.css'})
-      ],
-    }
+  if(process.env.NODE_ENV === 'production'){
+    return merge([
+      commonConfig(),
+      productionConfig()
+    ])
   }
 };
